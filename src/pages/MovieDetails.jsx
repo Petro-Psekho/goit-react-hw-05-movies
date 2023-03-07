2cbe6ca159fa8e3387da2647588a2087ad12f7ef
Petro Psekho <psekho.pp@gmail.com>
2023-03-07 15:45:39 +0200

    add markup movie details

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -11,18 +11,41 @@ export const MovieDetails = () => {
 
   useEffect(() => {
     getMovieDetails(movieId).then(data => {
+      console.log(data.genres);
       setMovieDetails(data);
     });
   }, [movieId]);
 
   return (
     <main>
-      {
-        <img
-          src={`${IMAGE_BASE_URL}${IMAGE_POSTER_SIZES}${movieDetails.poster_path}`}
-          alt=""
-        />
-      }
+      <div>
+        <div>
+          {
+            <img
+              src={`${IMAGE_BASE_URL}${IMAGE_POSTER_SIZES}${movieDetails.poster_path}`}
+              alt=""
+            />
+          }
+        </div>
+        <div>
+          <h2>{movieDetails.title}</h2>
+          <p>User Score: {Math.round(movieDetails.vote_average * 10)}%</p>
+          <h3>Overview</h3>
+          <p>{movieDetails.overview}</p>
+          <h4>Genres</h4>
+          <div>
+            {movieDetails.genres &&
+              movieDetails.genres.length &&
+              movieDetails.genres.map(({ id, name }) => (
+                <span key={id}>/ {name} /</span>
+              ))}
+          </div>
+        </div>
+      </div>
+      <div>
+        <p>_____________________________________________________________</p>
+        <h4>Additional information</h4>
+      </div>
     </main>
   );
 };
