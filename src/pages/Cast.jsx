47f4c255f0f1759a86a9a0fa4f6e2c95f378d186
Petro Psekho <psekho.pp@gmail.com>
2023-03-07 20:47:34 +0200

    adds no-image picture

--- src/pages/Cast.jsx
+++ src/pages/Cast.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getMovieCredits } from 'services/fetchMovies ';
+import noImage from 'img/stub.jpg';
 
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p';
 const IMAGE_POSTER_SIZES = '/w154/';
@@ -24,7 +25,11 @@ export const Cast = () => {
           movieCredits.cast.map(credits => (
             <li key={credits.id}>
               <img
-                src={`${IMAGE_BASE_URL}${IMAGE_POSTER_SIZES}${credits.profile_path}`}
+                src={
+                  credits.profile_path
+                    ? `${IMAGE_BASE_URL}${IMAGE_POSTER_SIZES}${credits.profile_path}`
+                    : `${noImage}`
+                }
                 alt=""
               />
               <h4>{credits.name}</h4>
