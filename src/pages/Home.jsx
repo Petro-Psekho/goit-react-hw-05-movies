5885c33501880d467f9d34bcad6735727ec019b8
Petro Psekho <psekho.pp@gmail.com>
2023-03-09 15:38:16 +0200

    fixes and update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,28 @@
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { getTrending } from 'services/fetchMovies ';
+import { LoaderSpinner } from 'components/LoaderSpinner/LoaderSpinner';
 
 const Home = () => {
   const [trendingMovies, setTrendingMovies] = useState([]);
+  const location = useLocation();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     getTrending().then(data => {
       setTrendingMovies(data.results);
+      setIsLoading(false);
     });
   }, []);
 
   return (
     <main>
       <ul>
+        {isLoading && <LoaderSpinner />}
         {trendingMovies.map(movie => (
           <li key={movie.id}>
-            <Link to={`/movies/${movie.id}`}>
+            <Link to={`/movies/${movie.id}`} state={{ from: location }}>
               <p>{movie.title}</p>
             </Link>
           </li>
